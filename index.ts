aacda61d1a9405ffa919b852138210e8d5bf456d
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-07-24 21:29:58 -0600

    :pencil: docs: add documentation in main class

--- index.ts
+++ index.ts
@@ -1,24 +1,74 @@
 import { promises as fs } from 'node:fs'
 import { join, basename, dirname } from 'node:path'
 
+/**
+ * Type definition for an async callback that takes no arguments and returns no value.
+ */
 export type Async = [() => void, (error: Error) => void] | null
+
+/**
+ * Type definition for a Promise that resolves to void or null.
+ */
 export type PromiseVoid = Promise<void> | null
 
+/**
+ * A class representing a JSON database.
+ * @template T The type of the data stored in the database.
+ */
 export class Ark<T = object | Array<any>> {
+  /**
+   * The data stored in the database.
+   */
   data: T | null = null
+
+  /**
+   * The path to the database file.
+   */
   #path: string
+
+  /**
+   * The path to the temporary file used for atomic writes.
+   */
   #temp: string
+
+  /**
+   * The callback to be called when the previous write operation completes successfully.
+   */
   #prev: Async = null
+
+  /**
+   * The callback to be called when the next write operation completes.
+   */
   #next: Async = null
+
+  /**
+   * Whether a write operation is currently in progress.
+   */
   #locked: boolean = false
+
+  /**
+   * The Promise that resolves when the current write operation completes.
+   */
   #promise: PromiseVoid = null
+
+  /**
+   * The data to be written in the next write operation.
+   */
   #payload: string | null = null
 
+  /**
+   * Creates a new Ark instance.
+   * @param db The path to the database file.
+   */
   constructor (db: string) {
     this.#path = !db.includes('.json') ? `${db}.json` : db
     db = join(process.cwd(), this.#path)
     this.#temp = this.#tempPath(this.#path)
   }
+
+  /**
+   * Connects to the database and loads its contents into memory.
+   */
   async connect () {
     let raw: string | null = null
     try { raw = await fs.readFile(this.#path, 'utf-8') }
@@ -33,12 +83,28 @@ export class Ark<T = object | Array<any>> {
     if (raw === null) this.data = null
     else this.data = JSON.parse(raw)
   }
+
+  /**
+   * Saves the current state of the database to disk.
+   */
   async save () {
     await this.#write(JSON.stringify(this.data))
   }
+
+  /**
+   * Returns the path to the temporary file used for atomic writes.
+   * @param file The path to the database file.
+   * @returns The path to the temporary file.
+   */
   #tempPath (file: string) {
     return join(dirname(file), '.' + basename(file) + '.tmp')
   }
+
+  /**
+   * Adds a write operation to the queue.
+   * @param data The data to be written.
+   * @returns A Promise that resolves when the write operation completes.
+   */
   async #push (data: string) {
     this.#payload = data
     this.#promise ||= new Promise((ok, err) => {
@@ -48,6 +114,11 @@ export class Ark<T = object | Array<any>> {
       this.#promise?.then(ok).catch(err)
     })
   }
+
+  /**
+   * Performs a write operation.
+   * @param data The data to be written.
+   */
   async #set (data: string) {
     this.#locked = true
     try {
@@ -70,6 +141,11 @@ export class Ark<T = object | Array<any>> {
       }
     }
   }
+
+  /**
+   * Writes data to the database.
+   * @param data The data to be written.
+   */
   async #write (data: string) {
     this.#locked ? this.#push(data) : this.#set(data)
   }
